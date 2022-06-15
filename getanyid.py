8d8f63e53f15a34a1c2189913d79c448a56eb12a
NAVANJANA <104069319+Captainamarica@users.noreply.github.com>
2022-06-15 20:17:33 +0530

    Update getanyid.py

--- getanyid.py
+++ getanyid.py
@@ -1,59 +1,27 @@
-#Copyright ©️ 2022 TeLe TiPs. All Rights Reserved
-#You are free to use this code in any of your project, but you MUST include the following in your README.md (Copy & paste)
-# ##Credits - [GetAnyID bot by TeLe TiPs] (https://github.com/teletips/GetAnyID-TeLeTiPs)
-
-from pyrogram import Client, filters
-from pyrogram.types import Message
+from pyrogram import Client, filters 
+from pyrogram.types import Chatpermission
+from pyrogram.types import InlinekeyboardButton, InlinekeyboardMarkup
 import os
 
-GetAnyIDBot=Client(
-    "GetAnyIDBot",
+bot=Client(
+    "Night Vission Bot",
     api_id = int(os.environ["API_ID"]),
     api_hash = os.environ["API_HASH"],
     bot_token = os.environ["BOT_TOKEN"]
 )
 
-@GetAnyIDBot.on_message(filters.command('start') & filters.private)
-async def start(client, message):
-    text = f"""
-Heya {message.from_user.mention},
-I am here to provide Telegram user and chat ID.
+START_MESSAGE = "Im Night Vission Group Help Bot"
+START_MESSAGE_BUTTONS = [
+[InlinekeyboardButton('SUPPORT', url='https://t.me/NightVisssion')
+]
+@bot.on_message(filters.command('start') & filters.private)
+def start(bot, message):
+	text = START_MESSAGE
+	reply_markup = InlinekeyboardMarkup(START_MESSAGE_BUTTONS)
+	message.reply(
+	text=text,
+	reply_markup=reply_markup
+	disable_web_page_preview=True
 
-To get ID, simply send the command <code>/id</code> directly to any chat or send it as a reply to any message.
-
-I can also work in group chats.
-
-🏠 | [Home](https://t.me/teletipsofficialchannel)
-            """
-    await GetAnyIDBot.send_message(message.chat.id, text, disable_web_page_preview=True)
-
-
-@GetAnyIDBot.on_message(filters.command('id'))
-async def get_id(client, message):
-    try:
-
-        if (not message.reply_to_message) and (message.chat):
-            await message.reply(f"User {message.from_user.first_name}'s ID is <code>{message.from_user.id }</code>.\nThis chat's ID is: <code>{message.chat.id}</code>.") 
-
-        elif not message.reply_to_message:
-            await message.reply(f"User {message.from_user.first_name}'s ID is <code>{message.from_user.id }</code>.") 
-
-        elif message.reply_to_message.forward_from_chat:
-            await message.reply(f"The forwarded {str(message.reply_to_message.forward_from_chat.type)[9:].lower()}, {message.reply_to_message.forward_from_chat.title} has an ID of <code>{message.reply_to_message.forward_from_chat.id}</code>.") 
-
-        elif message.reply_to_message.forward_from:
-            await message.reply(f"The forwarded user, {message.reply_to_message.forward_from.first_name} has an ID of <code>{message.reply_to_message.forward_from.id   }</code>.")
-
-        elif message.reply_to_message.forward_sender_name:
-            await message.reply("Sorry, you cannot get the forwarded user ID because of their privacy settings")
-
-        else:
-            await message.reply(f"User {message.reply_to_message.from_user.first_name}'s ID is <code>{message.reply_to_message.from_user.id}</code>.")   
-
-    except Exception:
-            await message.reply("An error occured while getting the ID.")
-
-print("GetAnyIDBot is alive!")
-GetAnyIDBot.run()
-
-#Copyright ©️ 2022 TeLe TiPs. All Rights Reserved
+print("Night Vission Bot Online Danger")
+bot.run()
